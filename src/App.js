bdd13473a5b8cdf33a4092cc8bb915d211c40555
boy1434@naver.com <boy1434@naver.com>
2023-12-11 23:04:07 +0900

    컴포넌트에 이벤트 만들기

--- src/App.js
+++ src/App.js
@@ -32,16 +32,13 @@ class App extends Component {
       }
       return (
         <div className="App">
-          {/* <Subject 
+          <Subject 
           title={this.state.subject.title}
           sub={this.state.subject.sub}
-          /> */}
-          <header>
-          <h1><a href="/" onClick={function (e){
-            e.preventDefault();
-          }}>{this.state.subject.title}</a></h1>
-         {this.state.subject.sub}
-        </header>
+          onChangePage={function() {
+            this.setState({mode:'welcome'})
+          }.bind(this)}
+          />
           <TOC data={this.state.contents}/>
           <Content title={_title} desc={_desc} />
       </div>

af4dd2cfb04d720ffb4c72e7cb5663f29d9900cb
boy1434@naver.com <boy1434@naver.com>
2023-12-17 18:26:00 +0900

    create: form 구현

--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -2,12 +2,35 @@
 function CreateContent(props) {
   console.log('Content render');
     return (
-      <header>
-      <h2>
-        Create
-      </h2>
-      {props.desc}
-      </header>
+      <article>
+        <h2>Create</h2>
+        
+        <form action="/create_process" method="post"
+          onSubmit={function(e){
+            e.preventDefault();
+            alert('submit');
+          }.bind(this)}
+        >
+          <p>
+            <input 
+              type="text" 
+              name="title" 
+              placeholder="Title">
+            </input>
+          </p>
+
+          <p>
+            <textarea 
+              name="desc"
+              placeholder="desscription">
+              </textarea>
+          </p>
+
+          <p>
+            <input type="submit"></input>
+          </p>
+        </form>
+      </article>
       
     )
   }

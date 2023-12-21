1d56070bf89b53f44a95dc559576e0043f5e9c06
boy1434@naver.com <boy1434@naver.com>
2023-12-22 00:05:09 +0900

    react_bootstrap 네브바 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React from "react";
 import Box from "./components/Box";
+import HeaderBar from "./components/HeaderBar";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import { Navbar } from "react-bootstrap";
 function App() {
   return(
-    <header>
+   <Router>
+    <HeaderBar/>
+      <Routes>
+        <Route path="/" element = {<Box/>}/>
 
-    <Box />
-    <Box />
-    <Box />
-    <Box />
-    </header>
+        
+      </Routes>
+   </Router>
   );
 }
 
